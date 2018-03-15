108ec9781d68bc45bf6cf7ad7b01435852a6ab65
Vidhya A <Vidhya.A@mea.ey.net>
2018-03-15 13:27:53 +0530

    Second version

--- script.py
+++ script.py
@@ -1,5 +1,16 @@
-# This program prints Hello, world!
+# Python program to swap two variables
 
+# To take input from the user
+# x = input('Enter value of x: ')
+# y = input('Enter value of y: ')
 
+x = 5
+y = 10
 
- print('Hello, world!')
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
